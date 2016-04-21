07dd1cc40493c661db87fc5e45119bc1d2540162
ray2233 <raymondparwez@yahoo.com>
2016-04-20 22:37:18 -0400

    First commit

---
+++ assets/js/config.js
@@ -0,0 +1,22 @@
+﻿var Healthcare = Healthcare || {};
+
+// Define application configuration
+Healthcare.config = {
+    resources: [{
+        folder: "i18n",
+        file: "resources.js"
+    }, {
+        folder: "data",
+        file: "data.js"
+    }],
+    language: {
+        defaultLanguage: "en",
+        supportedLanguages: ["en", "ja"]
+    },
+    reminder: {
+        snoozeDelayInMiliseconds: 30000,
+        delayBeforeShowingInMiliseconds: 30000
+    },
+    appJsFileLocation: "assets/js/app.js",
+    isWP: false
+}
